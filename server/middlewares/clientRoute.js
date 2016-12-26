7acf1dcf4d187aa8045604416b87849a42d630b4
chikara <chenhongtu@51xianqu.net>
2016-12-26 11:14:10 +0800

    Solve the denpendency from client side correctly

--- server/middlewares/clientRoute.js
+++ server/middlewares/clientRoute.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {renderToString} from 'react-dom/server'
 import {RouterContext, match} from 'react-router'
 import {Provider} from 'react-redux'
-import route from '../../client/route'
+import routes from '../../client/routes'
 import configureStore from '../../client/common/store/configureStore'
 
 const store = configureStore()
@@ -10,7 +10,7 @@ const store = configureStore()
 async function clientRoute(ctx, next) {
     let _renderProps
 
-    match({routes: route, location: ctx.url}, (error, redirectLocation, renderProps) => {
+    match({routes, location: ctx.url}, (error, redirectLocation, renderProps) => {
         _renderProps = renderProps
     })
 
