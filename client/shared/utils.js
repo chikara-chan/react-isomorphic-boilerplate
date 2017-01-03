aba13c998fbd7fb31935daa30d8007c017de86f1
chikara <chenhongtu@51xianqu.net>
2017-01-03 15:24:56 +0800

    Split date functions from the major utils and optimize the logics

--- client/shared/utils.js
+++ client/shared/utils.js
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 /**
  * @param  {Object} options
- * @return {Promise}
+ * @return {Object} Return Promise
  */
 function ajax(options) {
     const defaults = {
@@ -31,55 +31,7 @@ function ajax(options) {
 }
 
 /**
- * @param  {number} timestamp
- * @param  {String} format
- * @return {String}
- */
-function formatDate(timestamp, format) {
-    const date = new Date(timestamp),
-        years = `${date.getFullYear()}`,
-        month = `${date.getMonth()}`,
-        day = `${date.getDate()}`,
-        hours = `${date.getMonth()}`,
-        minutes = `${date.getMinutes()}`,
-        seconds = `${date.getSeconds()}`
-
-    return format.replace(/y+|M+|d+|h+|m+|s+/g, match => {
-        const length = match.length
-
-        switch (match.charAt(0)) {
-            case 'y':
-                return years.substr(-length)
-            case 'M':
-                return (month.length === 1 ? `0${month}` : month).substr(-length)
-            case 'd':
-                return (day.length === 1 ? `0${day}` : day).substr(-length)
-            case 'h':
-                return (hours.length === 1 ? `0${hours}` : hours).substr(-length)
-            case 'm':
-                return (minutes.length === 1 ? `0${minutes}` : minutes).substr(-length)
-            case 's':
-                return (seconds.length === 1 ? `0${seconds}` : seconds).substr(-length)
-        }
-    })
-}
-
-/**
- * @param  {String} str
- * @return {Date}
- */
-function parseDate(str) {
-    let results
-
-    if (results = str.match(/^\s*(\d{4})-(\d{1,2})-(\d{1,2})\s*$/)) {
-        return new Date(+results[1], +results[2] - 1, +results[3])
-    } else if (results = str.match(/^\s*(\d{4})-(\d{1,2})-(\d{1,2})\s+(\d{1,2}):(\d{1,2}):(\d{1,2})\s*$/)) {
-        return new Date(+results[1], +results[2] - 1, +results[3], +results[4], +results[5], +results[6])
-    }
-}
-
-/**
- * @return {Object}
+ * @return {Object} Return url params
  */
 function getUrlParams() {
     const search = location.search.slice(1),
@@ -96,7 +48,5 @@ function getUrlParams() {
 
 export default {
     ajax,
-    formatDate,
-    parseDate,
     getUrlParams
 }
