ab3575535ab84edc20127bd354c1128cca696971
chikara <chenhongtu@51xianqu.net>
2017-01-16 15:10:08 +0800

    Fix IndexLink component without href value can't be a link

--- client/common/components/Navbar.js
+++ client/common/components/Navbar.js
@@ -10,7 +10,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className={styles.navbar}>
-                <IndexLink className={styles.link} activeClassName={styles.active} to="">home</IndexLink>
+                <IndexLink className={styles.link} activeClassName={styles.active} to="/">home</IndexLink>
                 <Link className={styles.link} activeClassName={styles.active} to="explore">explore</Link>
                 <Link className={styles.link} activeClassName={styles.active} to="about">about</Link>
             </nav>
